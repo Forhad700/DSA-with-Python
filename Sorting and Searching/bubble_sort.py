916004c633d9d9b74204dc110f28c44407a4b5bc
Forhad <71933188+Forhad700@users.noreply.github.com>
2024-11-09 14:42:51 +0600

    Update bubble_sort.py

--- Sorting and Searching/bubble_sort.py
+++ Sorting and Searching/bubble_sort.py
@@ -1,19 +1,20 @@
 def bubble_sort(arr):
-    for i in range(len(arr)-1):
-        for j in range(len(arr)-1-i):
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
+    for i in range(len(arr)):
+        for j in range(1, len(arr)-i):
+            if arr[j-1]>arr[j]:
+                arr[j-1], arr[j] = arr[j], arr[j-1]
 
 n = int(input("Input size: "))
-lst = []
+arr = []
 for i in range(n):
-    val = int(input())         # or,  lst.append(int(input()))   
-    lst.append(val)
+    x = int(input())
+    arr.append(x)
+
 
 # # one line space-separated input  
 # n = int(input("Input size: "))
-# val = input("Enter space-separated values: ")
-# lst = list(map(int, val.split()))
+# x = input("Enter space-separated values: ")
+# arr = list(map(int, x.split()))
 
-bubble_sort(lst)
-print(lst)
+bubble_sort(arr)
+print(arr)
